ff553eb4964fbb5c99dbef683a0ed6d795b661f6
有種 <39034242+kenzok8@users.noreply.github.com>
2024-03-22 01:32:57 +0800

    Delete commit directory

--- commit/Modify.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# --------------------------------------------------------
-# Script for creating ACL file for each LuCI APP
-rm -rf ./*/.git ./*/.gitattributes ./*/.svn ./*/.github ./*/.gitignore create_acl_for_luci.err create_acl_for_luci.ok create_acl_for_luci.warn
-exit 0
